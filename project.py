6ea806f4f272b0e827689d7613ad60a16536643b
Oleksii Zhogan <alex-020203@ukr.net>
2016-03-31 15:55:48 +0300

    Updated proj.py impl and config files for project

--- project.py
+++ project.py
@@ -1,40 +1,45 @@
 # author: Oleksii Zhogan
 
+import os
 import sys
 
 from scripts.proj import Project
-from scripts.logger import log
-import os
 
-_DEF_PROJECT = "config/projects/demo.json"
-_ROOT = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+from scripts.logger import log
+
+
+selfPath = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+Project.intializePaths({
+    "config_root": selfPath + "/" + "config/projects",
+    "project_root": selfPath
+})
+_DEF_PROJECT = "config/projects/DemoApp.json"
+del selfPath
+
 
 def parseArgs():
-  from argparse import ArgumentParser
-  argPrs = ArgumentParser(description="Helpful script for project managment")
-  argPrs.add_argument("-p",       dest="proj",  nargs=1,   required=False, type=str,  default=_DEF_PROJECT,
+    from argparse import ArgumentParser
+    argPrs = ArgumentParser(description="Helpful script for project managment")
+    argPrs.add_argument("-p",     dest="proj",  nargs=1,   required=False, type=str,    default=_DEF_PROJECT,
                                   help="specifie custon project file")
-  argPrs.add_argument("-build",   dest="build", nargs="?", required=True,  type=str,  const="debug", choices=["debug", "release", "relwithdebinfo"],
-                                  help="build project of specifig type (debug, release, relwithdebinfo, etc.)")
-  argPrs.add_argument("-clean",   dest="clean", nargs="?", required=False, type=bool, const=True, default=False,
+    argPrs.add_argument("-build", dest="build", nargs="?", required=True,    type=str,    const="debug", choices=["debug", "release"],
+                                  help="build project of specifig type (debug, release)")
+    argPrs.add_argument("-clean", dest="clean", nargs="?", required=False, type=bool, const=True, default=False,
                                   help="clean all generated files before build")
-  return argPrs.parse_args()                                                                                                      
+    return argPrs.parse_args()                                                                                                                                                                                                            
 
 
 def main():
-  args = parseArgs()
-  try:
-    proj = Project(args.proj)
-    proj.setRoot(_ROOT)
-  except:
-    log.error("[Error] Can't load project from: {0}".format(args.proj))
-    log.debug("[Error] Problem: {0}".format(sys.exc_info()[1]))
-    return
-  log.info("[Info] Loaded project: {0}".format(proj.getName()))
-  if args.clean:
-    log.info("[Info] Remove all generated files")
-    proj.cleanUp()
-  proj.build(args.build)
+    args = parseArgs()
+    try:
+        proj = Project(args.proj)
+    except:
+        log.error("[Error] Unexpected error when loading project from: {0}".format(args.proj))
+        log.error("[Error] Problem: {0}".format(sys.exc_info()[1]))
+        return
+    if args.clean:
+        proj.cleanUp()
+    proj.build(args.build)
 
 if __name__ == "__main__":
-  main()
+    main()
--- scripts/proj.py
+++ scripts/proj.py
@@ -1,5 +1,3 @@
-# author: Oleksii Zhogan
-
 __all__ = ["Project"]
 
 import os
@@ -9,7 +7,8 @@ import json
 from logger import log
 
 from utils import runCMD
-from utils import setUpEnv
+
+_CMAKE_LOG_FILE = "lastCmakeRun.log"
 
 def _getPlatformInfo():
   import platform
@@ -28,98 +27,171 @@ Platform = _getPlatformInfo()
 del _getPlatformInfo
 
 
+class DependError(RuntimeError):
+    def __init__(self, depErr):
+        self._depErr = depErr
+    def __str__(self):
+        return  self._depErr
+
+
 class Project:
 
-  def __init__(self, filename):
-    self._model = dict()
-    self._rootPath = ""
-    self._loadModel(filename)
+    _CONFIG_ROOT = ""
+    _PROJECTS_ROOT = ""
+    
+    def __init__(self, configFile, parent=None):
+        self._parent = parent
+        self._depList = []
+        self._loadModel(configFile)
 
-  def setRoot(self, rootPath):
-    self._rootPath = rootPath
+    def getName(self):
+        return self._model["name"]
 
-  def _loadModel(self, filename):
-    with open(filename, "r") as tFile:
-      tModel = json.load(tFile)
-    self._model["name"] = tModel["name"]
-    self._model["location"] = tModel["location"]
-    if Platform["short_name"] != "win":
-      self._model["cmake"] = tModel["cmake"]["unix"]
-    else:
-      self._model["cmake"] = tModel["cmake"]["win"]
-    self._model["cmake_log"] = tModel["cmake"]["cmake_log"]
+    def cleanUp(self):
+        pass
+        
+    @staticmethod
+    def intializePaths(pathInfo):
+        Project._CONFIG_ROOT = pathInfo["config_root"]
+        Project._PROJECTS_ROOT = pathInfo["project_root"]
+        
+    def _absPath(self, path):
+       tPath = ""
+       if path != "":
+           tPath = "/" + path
+       return os.path.abspath(self._CONFIG_ROOT + tPath)
 
-  def getName(self):
-    return self._model["name"]
+    def build(self, buildType):
+        log.info("[Info][{0}] Start build target: {0}".format(self.getName()))
+        for depProj in self._depList:
+            if not depProj.build(buildType):
+                log.error("[Error][{0}] Can't build dep project: {1}".format(self.getName(), depProj.getName()))
+                return False
+        return self._doBuild(buildType)
 
-  def cleanUp(self):
-    # TODO: Implement this
-    pass
+    def _setBuildType(self, buildType):
+        pass
 
-  def _getGenerator(self):
-    return " -G\"{0}\"".format(self._model["cmake"]["gen"])
+    def _setLinkType(self, linkType):
+        pass
 
-  def _getBuildDir(self, forCmake=False):
-    tDirPath = self._rootPath + "/" + self._model["cmake"]["build_dir"] + "/" + self._model["name"]
-    if not os.path.exists(tDirPath):
-      os.makedirs(tDirPath)
-    if forCmake:
-      return " -B{0}".format(tDirPath)
-    return tDirPath
+    def _loadModel(self, filename):
+        try:
+            with open(filename, "r") as tFile:
+              self._model =  json.load(tFile)
+              if "deps" in self._model:
+                  for depItem in self._model.pop("deps"):
+                      self._depList.append(self._loadDepProject(depItem))
+        except DependError as depEX:
+            if self._parent is not None:
+                log.error("[Error][{0}] Problem: {1}".format(self.getName(), depEX))
+                raise DependError("Incomplete project: {0}".format(self.getName()))
+            else:
+               # can't format error suffix here, model not loaded
+               log.error("[Error][{0}] Problem: {1}".format(self._model["name"], depEX))
+               raise
+        except:
+            if self._parent is not None:
+                raise DependError("Can't load dep project from file: {0}".format(filename))
+            else:
+                log.error("[Error] Unknown error when load project from: {0}".format(filename))
+                raise
+        else:
+            log.info("[Info][{0}] Loaded -- OK".format(self.getName()))
 
-  def _setUpCmake(self):
-    enVars = self._model["cmake"]["ENVs"]
-    for item in enVars:
-      if "path" in item and item["path"]:
-        for elem in [x for x in item if x != "path"]:
-          tSuffix = ""
-          if item[elem] != "":
-            tSuffix = "/" + item[elem]
-          item[elem] = self._rootPath + tSuffix
-    resDict = dict()
-    for item in enVars:
-      for elem in item:
-        if elem != "path":
-           resDict[elem] = item[elem]
-    setUpEnv(resDict)
+    def _loadDepProject(self, depInfo):
+       tDepProj = Project(self._findProjectFile(depInfo), parent=self)
+       tDepProj._setBuildType(depInfo["type"])
+       tDepProj._setLinkType(depInfo["link"])
+       return tDepProj
 
-  def _getDefs(self):
-    if "defs" not in self._model["cmake"]:
-       return ""
-    tStr = ""
-    for item in self._model["cmake"]["defs"]:
-      tStr += " -D{0}".format(item)
-    return tStr
+    def _doBuild(self, buildType):
+        if not self._genCmakeProject(buildType):
+            log.error("[Error][{0}] Can't generate project by cmake. See log for more info".format(self.getName()))
+            return False
+        log.info("[Info][{0}] Cmake generation -- OK".format(self.getName()))
+        if not self._createBins(buildType):
+            log.info("[Error][{0}] Can't compile project. See log for more info".format(self.getName()))
+            return False
+        log.info("[Info][{0}] Compiled -- OK".format(self.getName()))
+        # Optional steps
+        if self._needInstall():
+            if not self._doInstall(buildType):
+                log.error("[Error][{0}] Can't install produced binaries. See log for more info".format(self.getName()))
+                return False
+            log.info("[Info][{0}] Install -- OK".format(self.getName()))
+        if self._needCleanAfterBuild(buildType):
+            if not self._doCleanAfterBuild(buildType):
+                log.error("[Error][{0}] Can't clean up after build".format(self.getName()))
+                return False
+            log.info("[Info][{0}] Clean up after build -- OK".format(self.getName()))
+        # All steps success
+        return True
 
-  def _getBuildDefs(self, buildType):
-    if buildType.lower() == "debug":
-      return " -DCMAKE_BUILD_TYPE=Debug"
-    else:
-      return " -DCMAKE_BUILD_TYPE=Release"
+    def _needInstall(self):
+        return False
 
-  def _runCmake(self, buildType):
-    if not self._setUpCmake():
-      pass
-    tCMD = "cmake -H." + self._getGenerator() + self._getBuildDir(forCmake=True) + self._getDefs() \
-                       + self._getBuildDefs(buildType)
-    tWorkDir = self._rootPath + "/" + self._model["location"]
-    tProcLog = self._getBuildDir() + "/" + self._model["cmake_log"]
-    with open(tProcLog, "w+") as tFile:
-      tProcData = runCMD(tCMD, pipe=tFile, isShell=Platform["shell"], workDir=tWorkDir)
-    tStatus = False
-    if tProcData["ret_code"] == 0:
-      tStatus = True
-    if tProcData["ret_code"] is None:
-      log.error("[Error] Invalid cmd data sent to cmake")
-    elif tProcData["ret_code"] != 0:
-      log.error("[Error] Invalid cmake files. See log for more info")
-    log.info("[Info] Cmake log saved to: {0}".format(tProcLog))
-    return tStatus
+    def _needCleanAfterBuild(self, buildType):
+        return False
 
-  def build(self, buildType):
-    if not self._runCmake(buildType):
-      log.error("[Error] Cmake generation failed. See cmake log for more info")
-    else:
-      log.info("[Info] Cmake generation successful")
+    def _runCmake(self, cmakeConfig):
+        tCmakeCmd = "cmake -H." + cmakeConfig["defs"]
+        tCmakeLogFile = cmakeConfig["run_dir"] + "/" + _CMAKE_LOG_FILE
+        try:
+            with open(tCmakeLogFile, "w+") as tFile:
+               cmakeOut = runCMD(tCmakeCmd, workDir=cmakeConfig["work_dir"], pipe=tFile, isShell=Platform["shell"])
+        except:
+            log.error("[Error][{0}] Can't run cmake: {1} ".format(self.getName(), sys.exc_info()[1]))
+            return False
+        else:
+            log.info("[Info][{0}] Cmake log saved to: {1}".format(self.getName(), tCmakeLogFile))
+            if cmakeOut["ret_code"] != 0:
+                return False
+            else:
+                return True
 
+    def _runCompiler(self, compileConfig):
+        return False
 
+    def _doInstall(self, buildType):
+        return False
+
+    def _doCleanAfterBuild(self, buildType):
+        return False
+
+    def _getCmakeRunDir(self):
+        return Project._PROJECTS_ROOT + "/" + self._model["cmake"]["run_dir"]
+
+    def _getCmakeDefs(self, buildType):
+        tCmake = self._model["cmake"]
+        return None
+
+    def _getCmakeEnvs(self, buildType):
+        return None
+
+    def _getCmakeOutDir(self, buildType):
+        return None
+
+    def _getCompileRunDir(self):
+        pass
+
+    def _genCmakeProject(self, buildType):
+        cmakeConfig = dict()
+        cmakeConfig["build_type"] = buildType
+        cmakeConfig["run_dir"] = self._getCmakeRunDir()
+        cmakeConfig["out_dir"] = self._getCmakeOutDir(buildType)
+        cmakeConfig["defs"] = self._getCmakeDefs(buildType)
+        cmakeConfig["envs"] = self._getCmakeEnvs(buildType)
+        return self._runCmake(cmakeConfig)
+
+    def _createBins(self, buildType):
+        compileConfig = dict()
+        compileConfig["build_type"] = buildType
+        compileConfig["run_dir"] = self._getCompileRunDir()
+        return self._runCompiler(compileConfig)
+        
+    def _findProjectFile(self, depInfo):
+        tPath = Project._CONFIG_ROOT + "/" + depInfo["project"]
+        if not os.path.exists(tPath):
+            raise DependError("Can't find project file: {0}".format(tPath))
+        return tPath
